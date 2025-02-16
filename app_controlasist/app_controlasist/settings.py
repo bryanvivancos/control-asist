d57285b09d096f58b1f965301b97bad9d6492620
bryan <bryanvivanco97@outlook.com>
2025-02-16 18:20:49 -0500

    conexion sql en proceso

--- app_controlasist/app_controlasist/settings.py
+++ app_controlasist/app_controlasist/settings.py
@@ -74,13 +74,27 @@ WSGI_APPLICATION = 'app_controlasist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'bdcasist',
+        'HOST': 'bryanZephyrus',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'unicode_results': True,
+            #'trusted_connection': 'yes',
+            'TrustServerCertificate': 'yes'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
